95fc425f2b4a19e3d67d84f8a29202c94cc36d57
Stephen Morman <stephen.morman@gmail.com>
2016-05-02 16:27:55 -0500

    Added a special case for "/test" route.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// bring in iron and its URL router 
+extern crate iron;
+extern crate router;
+
+// use specific parts 
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+fn main() {	
+	
+    let mut router = Router::new();  // Alternative syntax:
+    router.get("/", handler);        // let router = router!(get "/" => handler,
+    router.get("/:query", handler);  //                      get "/:query" => handler);
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+        if *query == "test" {
+			Ok(Response::with((status::Ok, "This is the test route."))) 
+        } else {
+        	Ok(Response::with((status::Ok, "Your route is: ".to_string() + *query)))             	
+        }
+    }
 }
+
