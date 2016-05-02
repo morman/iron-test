8aa7ee4769265ec44ebb5ad8340b032362bf8ca6
Stephen Morman <stephen.morman@gmail.com>
2016-05-02 16:31:16 -0500

    Code formatting fix.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-// bring in iron and its URL router 
+// bring in iron and its URL router
 extern crate iron;
 extern crate router;
 
-// use specific parts 
+// use specific parts
 use iron::prelude::*;
 use iron::status;
 use router::Router;
 
-fn main() {	
-	
+fn main() {
+
     let mut router = Router::new();  // Alternative syntax:
     router.get("/", handler);        // let router = router!(get "/" => handler,
     router.get("/:query", handler);  //                      get "/:query" => handler);
@@ -18,10 +18,9 @@ fn main() {
     fn handler(req: &mut Request) -> IronResult<Response> {
         let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
         if *query == "test" {
-			Ok(Response::with((status::Ok, "This is the test route."))) 
+            Ok(Response::with((status::Ok, "This is the test route.")))
         } else {
-        	Ok(Response::with((status::Ok, "Your route is: ".to_string() + *query)))             	
+            Ok(Response::with((status::Ok, "Your route is: ".to_string() + *query)))
         }
     }
 }
-
