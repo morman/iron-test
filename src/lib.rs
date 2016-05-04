35bc90eb84ea00f168e4793df1b74abf403fdc75
Stephen Morman <stephen.morman@gmail.com>
2016-05-03 22:34:27 -0500

    Added some tests: would that Windows's OpenSSL impl could validate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {}
+extern crate hyper;
+
+use hyper::*;
+
+#[test]
+fn get_test() {
+    let client = Client::new();
+
+    let res = client.get("http://localhost:3000/test").send().unwrap();
+
+    assert_eq!(res.status, hyper::Ok);
+    assert_eq!(res.body, "This is the test route");
+}
+
+#[test]
+fn get_not_test() {
+    let client = Client::new();
+
+    let res = client.get("http://localhost:3000/not_test").send().unwrap();
+
+    assert_eq!(res.status, hyper::Ok);
+    assert_eq!(res.body, "Your route is: not_test");
 }
